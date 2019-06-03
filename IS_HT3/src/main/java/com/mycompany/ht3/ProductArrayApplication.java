48e078141cb1efb1b4eea2a818d8a7429f2a9b7c
JOAN MANUEL DIAZ SALAZAR <jdiazs4@miumg.edu.gt>
2019-06-02 20:32:47 -0600

    Ejemplos en el main implementados

--- IS_HT3/src/main/java/com/mycompany/ht3/ProductArrayApplication.java
+++ IS_HT3/src/main/java/com/mycompany/ht3/ProductArrayApplication.java
@@ -14,7 +14,58 @@ import org.apache.commons.lang3.ArrayUtils;
  */
 public class ProductArrayApplication {
     public static void main(String args[]){
-       
+        long[] probando= null;
+        long[] resultado = null;
+        
+        //EJEMPLO 1 - PRUEBA UNITARIA 1
+        probando = new long[]{1, 2, 3, 4, 5};
+        resultado = calculateProductArrayBasic(probando);
+        System.out.println("Probando: " + Arrays.toString(probando) + " - Resultado: " + Arrays.toString(resultado));
+        
+        //EJEMPLO 2 - PRUEBA UNITARIA 2
+        probando = new long[]{};
+        resultado = calculateProductArrayBasic(probando);
+        System.out.println("Probando: " + Arrays.toString(probando) + " - Resultado: " + Arrays.toString(resultado));
+        
+        //EJEMPLO 3 - PRUEBA UNITARIA 3
+        probando = new long[]{0};
+        resultado = calculateProductArrayBasic(probando);
+        System.out.println("Probando: " + Arrays.toString(probando) + " - Resultado: " + Arrays.toString(resultado));
+        
+        //EJEMPLO 4 - PRUEBA UNITARIA 4
+        probando = new long[]{1};
+        resultado = calculateProductArrayBasic(probando);
+        System.out.println("Probando: " + Arrays.toString(probando) + " - Resultado: " + Arrays.toString(resultado));
+        
+        //EJEMPLO 5 - PRUEBA UNITARIA 5
+        probando = new long[]{1, 2};
+        resultado = calculateProductArrayBasic(probando);
+        System.out.println("Probando: " + Arrays.toString(probando) + " - Resultado: " + Arrays.toString(resultado));
+        
+        //EJEMPLO 6 - PRUEBA UNITARIA 6
+        probando = null;
+        resultado = calculateProductArrayBasic(probando);
+        System.out.println("Probando: " + Arrays.toString(probando) + " - Resultado: " + Arrays.toString(resultado));
+        
+        //EJEMPLO 7 - PRUEBA UNITARIA 7
+        probando = new long[0];
+        resultado = calculateProductArrayBasic(probando);
+        System.out.println("Probando: " + Arrays.toString(probando) + " - Resultado: " + Arrays.toString(resultado));
+        
+        //EJEMPLO 8 - PRUEBA UNITARIA 8
+        probando = new long[]{1, 0, 3, 0, 5};
+        resultado = calculateProductArrayBasic(probando);
+        System.out.println("Probando: " + Arrays.toString(probando) + " - Resultado: " + Arrays.toString(resultado));
+        
+        //EJEMPLO 9 - PRUEBA UNITARIA 9
+        probando = new long[]{1, 2, 3, 0, 5};
+        resultado = calculateProductArrayBasic(probando);
+        System.out.println("Probando: " + Arrays.toString(probando) + " - Resultado: " + Arrays.toString(resultado));
+        
+        //EJEMPLO 10 - PROPIA
+        probando = new long[]{1, 2, 3, 4, 5,6,7,8,9,10};
+        resultado = calculateProductArrayBasic(probando);
+        System.out.println("Probando: " + Arrays.toString(probando) + " - Resultado: " + Arrays.toString(resultado));
         
     }
     
